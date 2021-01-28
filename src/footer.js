3dcd2fb057f5fe574a589a21febed029f766bc68
Michael Lai <mrlai7782@gmail.com>
2021-01-28 00:33:21 -0800

    Placed nav buttons

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,6 @@
 function drawFooter() {
     const footer = document.createElement("footer");
     footer.classList.add("footer");
-    footer.textContent = "footer";
     content.appendChild(footer);
 }
 
--- src/header.js
+++ src/header.js
@@ -2,13 +2,23 @@
 function drawHeader() {
     const header = document.createElement("header"); // Draws header box
     header.classList.add("header");
-    header.textContent = "header";
     content.appendChild(header);
 
-    const nav = document.createElement("nav"); // Draws nav controls in header box
+    const nav = document.createElement("ul"); // Draws nav controls in header box
     nav.classList.add("nav");
-    nav.textContent = "nav";
     header.appendChild(nav);
+
+    function navButtons() { // Draws buttons of nav bar
+        const buttons = ["home", "menu", "about"] 
+        for (let i = 0; i < buttons.length; i++) {
+            const button = document.createElement("ul");
+            button.classList.add(`nav-${buttons[i]}`, "nav-Button");
+            button.textContent = buttons[i];
+            nav.appendChild(button);
+        }
+    }
+    navButtons();
+
 }
 
 
